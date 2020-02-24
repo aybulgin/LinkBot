f7bf3a778a62dffd4a50e02b711d77462036a93c
Taras Aiboulguine <taiboulguine@gmail.com>
2020-02-24 19:36:50 +0000

    settings goes to separate file

--- app.py
+++ app.py
@@ -7,14 +7,7 @@ from selenium.webdriver.firefox.options import Options
 import argparse
 import random
 from XPATHS import *
-
-
-HEADLESS = True
-SLEEP_SECONDS = 2
-RANDOM_PERCENTAGE = 0.3
-KILLSWITCH = False#If true we stop sending connection invites
-URL = "https://www.linkedin.com"
-KEYWORD_SEARCH_URL = "https://www.linkedin.com/search/results/people/?keywords="
+from settings import *
 
 
 def wait():
---
+++ settings.py
@@ -0,0 +1,7 @@
+HEADLESS = True
+SLEEP_SECONDS = 2
+RANDOM_PERCENTAGE = 0.3
+KILLSWITCH = False #If true we stop sending connection invites
+
+URL = "https://www.linkedin.com"
+KEYWORD_SEARCH_URL = "https://www.linkedin.com/search/results/people/?keywords="
