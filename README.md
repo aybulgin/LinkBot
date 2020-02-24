1c3fbeeda7a6d1598035fbdd656f201e0e23e1b2
Taras Aiboulguine <taiboulguine@gmail.com>
2020-02-24 14:39:11 +0000

    0.2 added profile viewer and mode switch argument

--- README.md
+++ README.md
@@ -10,14 +10,21 @@ If you have these installed and the firefox driver (geckodriver.exe) in PATH, ru
 
 ## Running the script
 After the installation you can run the script with the following command in the console
-```python app.py <your@email.com> <password> <keyword> <pages>```
+- ```python app.py <your@email.com> <password> <keyword> <pages> <mode>```
 ### Arguments
--Email (Your linkedin login)
--Password (Your linkedin password)
--Keyword (What keyword related profiles you want to search. Ex: founder)
--Pages (How many pages do you want to look for new connections)
+- Email (Your linkedin login)
+- Password (Your linkedin password)
+- Keyword (What keyword related profiles you want to search. Ex: founder)
+- Pages (How many pages do you want to look for new connections)
+- Mode
+   MODE 1: Send invites to profiles based on people keyword search  
+   MODE 2: Profile viewer based on on people keyword search  
 
 The script will open Firefox and look for people related to your keyword then send them a connection invite.
+
+## Changelog
+- 0.2 - Added a profile viewer and Mode argument to switch between functions
+- 0.1 - Simple script to send connection invites 
 
 ## Disclaimer
 USE AT YOUR OWN RISK.
--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import argparse
 
 SLEEP_SECONDS = 2
 
-LOGIN_URL = "https://www.linkedin.com/login"
+URL = "https://www.linkedin.com"
 KEYWORD_SEARCH_URL = "https://www.linkedin.com/search/results/people/?keywords="
 
 XPATH_LOGIN_EMAIL = "//*[@id='username']"
@@ -19,13 +19,15 @@ XPATH_SKIP_ADD_PHONE = "//button[contains(text(),'Skip')]"
 XPATH_SEARCH_CONNECT_BUTTON = "//button[contains(@class,'search-result') and contains(.,'Connect')]"
 XPATH_SEARCH_CONNECT_CONFIRM = "//button[contains(.,'Send now')]"
 
+XPATH_PROFILE_URL = "//a[contains(@class,'search-result') and contains(@href,'/in/') and not(contains(@class,'social-proof'))]"
+
 
 def wait():
 	sleep(SLEEP_SECONDS)
 
 
 def start_login(driver, email, password, keyword):
-	try: driver.get(LOGIN_URL)
+	try: driver.get(URL + "/login")
 	except TimeoutException: start_login(driver, email, password, keyword)
 
 	driver.find_element(By.XPATH, XPATH_LOGIN_EMAIL).send_keys(email)
@@ -38,27 +40,47 @@ def start_login(driver, email, password, keyword):
 		driver.find_element(By.XPATH, XPATH_SKIP_ADD_PHONE).click()
 
 
-def keyword_search(driver, keyword, pages):
+#Scrape profile links in current page and visits them
+def profile_viewer(driver):
+	profile_links = set(driver.find_elements(By.XPATH, XPATH_PROFILE_URL))
+	
+	for profile_link in {link.get_attribute("href") for link in profile_links}:
+		print(f'View: {profile_link}')
+		driver.get(profile_link)
+		wait()
+
+
+#Find all connect buttons and click them and confirm Send Now
+def send_connection_invites(driver):
+	connect_buttons = driver.find_elements(By.XPATH, XPATH_SEARCH_CONNECT_BUTTON)
+
+	for connect in connect_buttons:
+		connect.click()
+		driver.find_element(By.XPATH, XPATH_SEARCH_CONNECT_CONFIRM).click()
+		print('Connection Invite Sent')
+		wait()
+
+
+#Loads keyword search
+def keyword_search(driver, keyword, pages, mode):
 	driver.get(f'{KEYWORD_SEARCH_URL}{keyword}')
 
 	for page in range(2, pages + 1):
-		connect_buttons = driver.find_elements(By.XPATH, XPATH_SEARCH_CONNECT_BUTTON)
-
-		#For each page, find all connect buttons and click them, then confirm Send Now
-		for connect in connect_buttons:
-			connect.click()
-			driver.find_element(By.XPATH, XPATH_SEARCH_CONNECT_CONFIRM).click()
-
-			print('Connection Invite Sent')
-			wait()
+		#Takes different actions based on MODE
+		if mode == 1:
+			send_connection_invites(driver)
+		elif mode == 2:
+			profile_viewer(driver)
+		else: break
 
 		driver.get(f'{KEYWORD_SEARCH_URL}{keyword}&page={page}')
 
 
-def main(email, password, keyword, pages):
+def main(email, password, keyword, pages, mode):
 	driver = webdriver.Firefox(executable_path='geckodriver.exe')
+
 	start_login(driver, email, password, keyword)
-	keyword_search(driver, keyword, pages)
+	keyword_search(driver, keyword, pages, mode)
 
 
 if __name__=="__main__":
@@ -67,11 +89,13 @@ if __name__=="__main__":
 	argparser.add_argument('password', help = 'Login Password')
 	argparser.add_argument('keyword', help = 'Search Keyword')
 	argparser.add_argument('pages', help = 'How many pages to search', type=int)
+	argparser.add_argument('mode', help = 'What mode to run the script on', type=int)
 
 	args = argparser.parse_args()
 	email = args.email
 	password = args.password
 	keyword = args.keyword
 	pages = args.pages
-
-	main(email, password, keyword, pages)
+	mode = args.mode
+	
+	main(email, password, keyword, pages, mode)
