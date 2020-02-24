dd7d39d7ce96d7b7eb4b18730d5dc6ae518b5662
Taras Aiboulguine <taiboulguine@gmail.com>
2020-02-24 15:00:31 +0000

    xpath variables goes to a separate file

---
+++ XPATHS.py
@@ -0,0 +1,10 @@
+XPATH_LOGIN_EMAIL = "//*[@id='username']"
+XPATH_LOGIN_PASSWORD = "//*[@id='password']"
+XPATH_LOGIN_SUBMIT = "//button[@type='submit']"
+
+XPATH_SKIP_ADD_PHONE = "//button[contains(text(),'Skip')]"
+
+XPATH_SEARCH_CONNECT_BUTTON = "//button[contains(@class,'search-result') and contains(.,'Connect')]"
+XPATH_SEARCH_CONNECT_CONFIRM = "//button[contains(.,'Send now')]"
+
+XPATH_PROFILE_URL = "//a[contains(@class,'search-result') and contains(@href,'/in/') and not(contains(@class,'social-proof'))]"
--- app.py
+++ app.py
@@ -10,17 +10,6 @@ SLEEP_SECONDS = 2
 URL = "https://www.linkedin.com"
 KEYWORD_SEARCH_URL = "https://www.linkedin.com/search/results/people/?keywords="
 
-XPATH_LOGIN_EMAIL = "//*[@id='username']"
-XPATH_LOGIN_PASSWORD = "//*[@id='password']"
-XPATH_LOGIN_SUBMIT = "//button[@type='submit']"
-
-XPATH_SKIP_ADD_PHONE = "//button[contains(text(),'Skip')]"
-
-XPATH_SEARCH_CONNECT_BUTTON = "//button[contains(@class,'search-result') and contains(.,'Connect')]"
-XPATH_SEARCH_CONNECT_CONFIRM = "//button[contains(.,'Send now')]"
-
-XPATH_PROFILE_URL = "//a[contains(@class,'search-result') and contains(@href,'/in/') and not(contains(@class,'social-proof'))]"
-
 
 def wait():
 	sleep(SLEEP_SECONDS)
